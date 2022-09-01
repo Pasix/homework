17f97f8ac5be3952a380ac1ba2ac9d0a81156122
Pasix <vadimpaseev@gmail.com>
2022-09-01 18:58:27 +0300

    заглушка 3 дз

---
+++ script.js
@@ -0,0 +1,24 @@
+// 'use strict'
+
+// let myBasket = [
+//     ['Трусы', 3, 500],
+//     ['Носки', 5, 300],
+//     ['Майка', 2, 400],
+//     ['Галстук', 0, 1000],
+//     ['Перчатки', 1, 500],
+// ]
+
+// console.log(myBasket);
+
+
+// function countBasketPrice(i) {
+//     let myBasketPrice = 0;
+
+
+//     for (i = 2; i < myBasket.length; i++) {
+//         myBasketPrice = myBasketPrice + myBasket[i][1] * myBasket[i][2];
+//     }
+//     console.log(myBasketPrice);
+// }
+
+// countBasketPrice(2);
